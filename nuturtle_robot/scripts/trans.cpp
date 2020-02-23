150ad556a2533f6fc7a693adc5fcbe1071df523e
luxi-huang <lucy0307@qq.com>
2020-02-22 20:42:37 -0600

    update

--- nuturtle_robot/scripts/trans.cpp
+++ nuturtle_robot/scripts/trans.cpp
@@ -28,12 +28,12 @@ using rigid2d::PI;
 
 ros::Publisher cmd_vel_publisher;
 ros::ServiceServer service;
-int rotation_sign =1;
+int rotation_sign =-1;
 
 void publish_velocity(double rotation_velocity);
 
 bool start_callback(nuturtle_robot::start::Request  &req,
-                  nuturtle_robot::start::Response &resp)
+                    nuturtle_robot::start::Response &resp)
 {
   if (req.clockwise_forward == 1){
     rotation_sign = 1;
@@ -43,9 +43,6 @@ bool start_callback(nuturtle_robot::start::Request  &req,
   return 1;
 }
 
-
-
-
 int main(int argc, char **argv)
 {
   ros::init(argc, argv, "rotation");
@@ -53,17 +50,16 @@ int main(int argc, char **argv)
   cmd_vel_publisher = n.advertise<geometry_msgs::Twist>("/turtle1/cmd_vel", 1000);
 
   double frac_vel = 0.8;
-  double maximum_rotational_velocity_robot = 2.84;
+  double maximum_translational_velocity = 0.22;
   n.getParam("frac_vel",frac_vel);
-  n.getParam("maximum_rotational_velocity_robot", maximum_rotational_velocity_robot);
+  n.getParam("maximum_translational_velocity", maximum_translational_velocity);
   ros::ServiceServer service = n.advertiseService("/start", start_callback);
 
-  double rotation_velocity = frac_vel*maximum_rotational_velocity_robot;
-  ROS_INFO("rotation_velocity:%f", rotation_velocity);
-  double rotation_time = 2.0*PI/rotation_velocity;
-  ROS_INFO("rotation_time:%f", rotation_time);
-  double pub_intervel = 0.02;
-  double loop_number_double = rotation_time / pub_intervel;
+  double translation_velocity = frac_vel*maximum_translational_velocity;
+  ROS_INFO("rotation_velocity:%f", translation_velocity);
+  double translation_time = 0.2/translation_velocity;
+  double pub_intervel = 0.005;
+  double loop_number_double = translation_time / pub_intervel;
   ROS_INFO("loop_number_double%f",loop_number_double);
   int loop_number_int = round(loop_number_double);
   ROS_INFO("loop_number_int%d",loop_number_int);
@@ -75,18 +71,18 @@ int main(int argc, char **argv)
   ros::Rate rate2(0.1);
   rate2.sleep();
 
-  for (int j =0; j<20; j++)
+  for (int j =0; j<10; j++)
   {
-    ROS_INFO("rotate!!!ya");
+    ROS_INFO("rotate!!!!ya");
     for(int i = 0; i<loop_number_int;i++)
     {
-      publish_velocity(rotation_sign*rotation_velocity);
+      publish_velocity(translation_velocity);
         // ROS_INFO("after_publish");
       rate.sleep();
 
     }
 
-    for(int i = 0; i<loop_number_int/20;i++)
+    for(int i = 0; i<loop_number_int/10;i++)
     {
         publish_velocity(0);
         // ROS_INFO("stop");
@@ -99,16 +95,15 @@ int main(int argc, char **argv)
 
 
 
-void publish_velocity(double rotation_velocity){
-  geometry_msgs::Twist rotation_angle;
-  rotation_angle.linear.x = 0;
-  rotation_angle.linear.x = 0;
-  rotation_angle.linear.x = 0;
+void publish_velocity(double translation_velocity){
+  geometry_msgs::Twist translation;
+  translation.linear.x = translation_velocity;
+  translation.linear.y = 0;
+  translation.linear.z = 0;
 
-  rotation_angle.angular.x = 0;
-  rotation_angle.angular.y = 0;
-  rotation_angle.angular.z = rotation_velocity;
+  translation.angular.x = 0;
+  translation.angular.y = 0;
+  translation.angular.z = 0;
   // ROS_INFO ("%f",rotation_angle.angular.z);
-
-  cmd_vel_publisher.publish(rotation_angle);
+  cmd_vel_publisher.publish(translation);
 }
--- rigid2d/scripts/odometer.cpp
+++ rigid2d/scripts/odometer.cpp
@@ -55,15 +55,12 @@ DiffDrive diffa;
 bool set_pose_callback(rigid2d::set_pen::Request  &req,
                 rigid2d::set_pen::Response &resp)
 {
-  // ros::NodeHandle n;
-  // odm_publisher = n.advertise<nav_msgs::Odometry>("Odometry/Odometry", 1000);
   Pose p;
   ros::Time current_time;
   Twist2D tw;
   tw.vx = 0;
   tw.vy = 0;
   tw.theta_dot = 0;
-  ROS_INFO("service 111111");
 
   current_time = ros::Time::now();
   p.x = req.robot_pose.x;
@@ -80,7 +77,6 @@ bool set_pose_callback(rigid2d::set_pen::Request  &req,
   diffa= DiffDrive(p,wheel_base,wheel_radius);
   send_TF(p, current_time);
   pub_odm (p,tw, current_time);
-  ROS_INFO("service works!");
 
   return 1;
 }
@@ -133,21 +129,8 @@ int main(int argc, char **argv)
   current_time = ros::Time::now();
   last_time = current_time;
 
-  //
-  // Pose new_pose;
-  // new_pose.x = 4;
-  // new_pose.y = 4;
-  // new_pose.theta = 0;
-  // ROS_INFO("BEFORE SERVICE");
-  //
-  // set_pose_client(new_pose);
-  // ROS_INFO("AFTER SEVICE");
-
-  // ros::Rate loop_rate(10);
   while(ros::ok()){
     ros::spinOnce();
-    // ROS_INFO("wheel_bas%f ",wheel_base);
-    // ROS_INFO("INSIDE LOOP");
     current_time = ros::Time::now();
     duration = (current_time - last_time).toSec();
     last_time = current_time;
@@ -159,16 +142,11 @@ int main(int argc, char **argv)
     P = diffa.pose();
     send_TF(P, current_time);
     pub_odm (P,tw, current_time);
-    // loop_rate.sleep();
-
   }
 
-//
-//
   return 0;
 }
-//
-//
+
 void poseCallback(const sensor_msgs::JointState::ConstPtr & mesg){
 	wheel1.name = mesg->name[0];
   wheel2.name = mesg->name[1];
@@ -187,8 +165,7 @@ WheelVelocities wheel_vel(){
   w.u4 = w.u1;
   return w;
 }
-//
-// //
+
 void send_TF(Pose P,ros::Time current_time){
   static tf2_ros::TransformBroadcaster odom_broadcaster;
   geometry_msgs::TransformStamped odom_trans;
@@ -208,7 +185,7 @@ void send_TF(Pose P,ros::Time current_time){
 
   odom_broadcaster.sendTransform(odom_trans);
 }
-// //
+
 void pub_odm (Pose P,Twist2D tw,ros::Time current_time){
   geometry_msgs::Quaternion odom_quat = tf::createQuaternionMsgFromYaw(P.theta);
   nav_msgs::Odometry odom;
@@ -227,25 +204,3 @@ void pub_odm (Pose P,Twist2D tw,ros::Time current_time){
   odom.twist.twist.angular.z = tw.theta_dot;
   odm_publisher.publish(odom);
 }
-
-
-// int set_pose_client(Pose pp){
-//   ros::NodeHandle nh1;
-//   ros::ServiceClient client= nh1.serviceClient<rigid2d::set_pen>("/set_pose", 1000);
-//   rigid2d::set_pen srv;
-//   srv.request.robot_pose.x = pp.x;
-//   srv.request.robot_pose.y = pp.y;
-//   srv.request.robot_pose.theta = pp.theta;
-//
-//   ros::service::waitForService("set_pose_service");
-//   if (client.call(srv))
-//           {
-//                   ROS_INFO("CALL set_pose");
-//           }
-//           else
-//           {
-//                   ROS_ERROR("Failed to call service teleport");
-//                   return 1;
-//           }
-//   return 0;
-// }
